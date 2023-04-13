3c53e07cc6d9ef6f3e0ced25c676b543278eafe2
Sahu, Anish EX1 <anish.sahu@iqvia.com>
2023-04-13 11:56:24 +0000

    Pd 3261 cdc

--- etmfa/server/namespaces/cdc_util.py
+++ etmfa/server/namespaces/cdc_util.py
@@ -16,27 +16,36 @@ class CdcThread(Thread):
     def __init__(self, w_id):
         Thread.__init__(self)
         self.w_id = w_id
+        self._is_running = True
+
 
     def run(self):
-        from etmfa.server import app
-        engine = db_context.get_engine(app)
-        session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-        try:
-            db_url = Config.SQLALCHEMY_DATABASE_URI
+        while (self._is_running):
+            from etmfa.server import app
+            engine = db_context.get_engine(app)
+            session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+            try:
+                db_url = Config.SQLALCHEMY_DATABASE_URI
 
-            with session_local() as session:
-                latest_work = session.query(WorkFlowStatus).filter_by(work_flow_id=self.w_id).first()
-                try:
-                    run_cdc('audit',db_url)
-                    latest_work.status = "COMPLETED"
-                    session.commit()
-                    session.close()
-                except Exception as _:
-                    latest_work.status = "ERROR"
-                    session.commit()
-                    session.close()
+                with session_local() as session:
+                    latest_work = session.query(WorkFlowStatus).filter_by(work_flow_id=self.w_id).first()
+                    try:
+                        run_cdc('audit',db_url)
+                        latest_work.status = "COMPLETED"
+                        session.commit()
+                        session.close()
+                        logger.info("CDC enabled successfully.")
+                    except Exception as err:
+                        latest_work.status = "ERROR"
+                        session.commit()
+                        session.close()
+                        logger.error(f"Error in running CDC function - {err}.")
 
-        except Exception as exp:
-            logger.error(
-                "Loading Defaults due to exception when reading yaml from server_config {0}"+str(exp))
+            except Exception as exp:
+                logger.error(
+                    "Loading Defaults due to exception when reading yaml from server_config {0}"+str(exp))
 
+            self.stop()
+
+    def stop(self):
+        self._is_running = False
