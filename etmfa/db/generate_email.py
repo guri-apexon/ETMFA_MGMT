51fdd414f0c5d89a6660f63af3ea535d4a29c7d4
Sugappa, Mouneshwara EX1 <mouneshwara.sugappa@iqvia.com>
2023-04-25 11:33:57 +0000

    Feature/pd 4256 alm

--- etmfa/db/generate_email.py
+++ etmfa/db/generate_email.py
@@ -24,7 +24,9 @@ from etmfa.db.models.pd_protocol_metadata import PDProtocolMetadata
 from etmfa.db.models.pd_user_protocols import PDUserProtocols
 from etmfa.db.models.pd_users import User
 from flask import Response
-from etmfa.server.config import Config 
+from etmfa.server.config import Config
+from jinja2 import Template
+
 
 logger = logging.getLogger(consts.LOGGING_NAME)
 
@@ -192,17 +194,83 @@ def send_mail_on_edited_event(db: db_context):
     """
         created function for tiggering email for EDITED event
     """
-    
+    event = "EDITED"
     doc_ids = [doc_record[0] for doc_record in db.query(Protocolalert).filter(
         Protocolalert.emailSentFlag == False,
         Protocolalert.email_template_id == '3',
         Protocolalert.timeUpdated >= datetime.today().date(),
     ).with_entities(Protocolalert.aidocId).all()]
-    
-    for doc in set(doc_ids):
+
+    event_dict = EVENT_CONFIG.get(event)
+
+    row_data = db.query(PDUserProtocols.id, PDProtocolMetadata.protocol,
+                        PDProtocolMetadata.protocolTitle,
+                        PDProtocolMetadata.indication,
+                        PDProtocolMetadata.status,
+                        PDProtocolMetadata.qcStatus,
+                        PDProtocolMetadata.versionNumber,
+                        PDProtocolMetadata.documentStatus,
+                        PDUserProtocols.userId,
+                        PDProtocolMetadata.id.label("doc_id"),
+                        User.username,
+                        User.email).join(PDUserProtocols, and_(PDProtocolMetadata.id.in_(list(set(doc_ids))),
+                                                               PDProtocolMetadata.protocol == PDUserProtocols.protocol)).join(
+        User, User.username.in_(('q' + PDUserProtocols.userId, 'u' + PDUserProtocols.userId,
+                                 PDUserProtocols.userId))).filter_by(**event_dict).all()
+
+    html_record = db.query(PdEmailTemplates).filter(
+        PdEmailTemplates.event == event).first()
+
+    subject = html_record.subject
+    email_dict = {}
+    for row in row_data:
+        record = email_dict.get(row.email, [])
+        if record:
+            email_dict.get(row.email).append({
+                "to_mail": row.email,
+                "username": " ".join(row.email.split("@")[0].split(".")).title(),
+                "doc_link": f"{Config.UI_HOST_NAME}/protocols?protocolId={row.doc_id}",
+                "protocol_number": row.protocol,
+                "indication": row.indication,
+                "doc_status": row.documentStatus,
+                "doc_activity": row.status,  
+                "version_number": row.versionNumber,
+            })
+        else:
+            email_dict[row.email] = [{
+                "to_mail": row.email,
+                "username": " ".join(row.email.split("@")[0].split(".")).title(),
+                "doc_link": f"{Config.UI_HOST_NAME}/protocols?protocolId={row.doc_id}",
+                "protocol_number": row.protocol,
+                "indication": row.indication,
+                "doc_status": row.documentStatus,
+                "doc_activity": row.status,
+                "version_number": row.versionNumber,
+            }]
+
+        time_ = datetime.now(timezone.utc)
+        db.query(Protocolalert).filter(Protocolalert.id == row.id, Protocolalert.aidocId == row.doc_id, Protocolalert.protocol == row.protocol).update({Protocolalert.emailSentFlag: True,
+                                                                                                                                                        Protocolalert.timeUpdated: time_, Protocolalert.emailSentTime: time_})
+        db.commit()                                                                                                                                               
+    # remove duplicate dict in list
+    result_dict = {}
+    for k, v in email_dict.items():
+        new_list = []
+        for d in v:
+            t = tuple(sorted(d.items()))
+            if t not in new_list:
+                new_list.append(t)
+        result = [dict(t) for t in new_list]
+        result_dict[k] = result
+
+    for email, email_val in result_dict.items():
         try:
-            response = send_event_based_mail(db, doc, "EDITED", True)
-            logger.info(f"mail sent {response} for doc id {doc}")
+            html_body = Template(html_record.email_body).render(
+                **{"username": email_val[0].get("username"), "email_body": email_val})
+            send_mail(subject, email, html_body, False)
+            logger.info(
+                f"Edit event mail sent success {str(datetime.now())}")
         except Exception as ex:
-            logger.error(f"error occured {doc} as {str(ex)}")
+            logger.exception(
+                f"Exception occured to send email exception {ex} on {str(datetime.now())}")
     return {"message":"Email process has been completed successfully"}
