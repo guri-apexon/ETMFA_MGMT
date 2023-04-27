ae08d05ad5359b15d35f9843bd908f6089e3bc2d
Sugappa, Mouneshwara EX1 <mouneshwara.sugappa@iqvia.com>
2023-04-27 13:43:53 +0000

    Defect/alm 699 new

--- etmfa/tests/unit/test_email_notification.py
+++ etmfa/tests/unit/test_email_notification.py
@@ -4,6 +4,6 @@ from microservices.email_notification import EmailNotification
 def test_email_notification():
     cc=Config('simple')
     fn=EmailNotification(cc)
-    data={'final':{'docId':'4c7ea27b-8a6b-4bf0-a8ed-2c1e49bbdc8c'}}
+    data={'final':{'docId':'4c7ea27b-8a6b-4bf0-a8ed-2c1e49bbdc8c','test_case':True}}
     msg=fn.on_callback(data)
     assert msg!=None
