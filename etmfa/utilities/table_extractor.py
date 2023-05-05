81cbbf97b21fe2176c5fd1ab9c8f819017b2a3a7
Patel, Urvesh EX1 <urvesh.patel@iqvia.com>
2023-05-05 15:21:25 +0000

    Feature/defect709 new

--- etmfa/utilities/table_extractor.py
+++ etmfa/utilities/table_extractor.py
@@ -204,6 +204,7 @@ class SOAResponse:
                         Metalist['AttachmentListProperties'] = Metalist.get('AttachmentList', list())
                         del Metalist['AttachmentList']
 
+
             result=[]
             if len(df) > 1:
                 df[['TableIndex','RowIndex','ColIndex']] = df[['TableIndex','RowIndex','ColIndex']].astype(float)
@@ -249,48 +250,44 @@ class SOAResponse:
             return ({}, 0)
 
 
-def get_row_id(item):
-    """ To get row id from any one cell """
-    for _, value in item.items():
-        if isinstance(value, dict):
-            roi_id = value.get("roi_id", {}).get('row_roi_id')
-            return roi_id
-
-
 def return_table_formatted_data(data):
-    table_properties_formatter = []
+    table_properties_formatter, datacell_roi_ids  = [], []
     for item in eval(data):
         column_list = []
-        roi_id = get_row_id(item=item)
         for k, v in item.items():
-            if isinstance(v, dict):
-                column_list.append({
-                          "col_indx": len(column_list),
-                          "op_type": None,
-                          "cell_id": v.get("roi_id",{}).get("datacell_roi_id",""),
-                          "value": v.get('content').replace("\\","")
+            if v:
+                roi_id = v.get("roi_id", {}).get('row_roi_id')
+                if isinstance(v, dict):
+                    datacell_id = v.get("roi_id", {}).get('datacell_roi_id', "")
+                    if datacell_id and datacell_id not in datacell_roi_ids:
+                        datacell_roi_ids.append(datacell_id)
+                        column_list.append({
+                            "col_indx": len(column_list),
+                            "op_type": None,
+                            "cell_id": v.get("roi_id", {}).get("datacell_roi_id", ""),
+                            "value": v.get('content').replace("\\","")
                         })
-            elif roi_id:
-                # To add cell id if it has missed during section extract
-                column_objs = db_context.session.query(DocumenttablesDb).filter(
-                    DocumenttablesDb.parent_id == roi_id).all()
-                missing_col_obj = column_objs[int(k.split('.')[0])-1]
-                cell_id = db_context.session.query(DocumenttablesDb).filter(
-                    DocumenttablesDb.parent_id == missing_col_obj.id).all()[-1]
-                column_list.append({
-                          "col_indx": len(column_list),
-                          "op_type": None,
-                          "cell_id": cell_id.id,
-                          "value": ""
-                        })
-            else:
-                # To handle if there is no data for entire row
-                column_list.append({
-                    "col_indx": len(column_list),
-                    "op_type": None,
-                    "cell_id": "",
-                    "value": ""
-                })
+                elif roi_id and v and v.get("roi_id", {}).get("datacell_roi_id", ""):
+                    # To add cell id if it has missed during section extract
+                    column_objs = db_context.session.query(DocumenttablesDb).filter(
+                        DocumenttablesDb.parent_id == roi_id).all()
+                    missing_col_obj = column_objs[int(k.split('.')[0])-1]
+                    cell_id = db_context.session.query(DocumenttablesDb).filter(
+                        DocumenttablesDb.parent_id == missing_col_obj.id).all()[-1]
+                    column_list.append({
+                              "col_indx": len(column_list),
+                              "op_type": None,
+                              "cell_id": cell_id.id,
+                              "value": ""
+                            })
+                else:
+                    # To handle if there is no data for entire row
+                    column_list.append({
+                        "col_indx": len(column_list),
+                        "op_type": None,
+                        "cell_id": "",
+                        "value": ""
+                    })
 
         new_dict = {"row_indx": len(table_properties_formatter),
                     "roi_id": roi_id, "op_type": None, "columns": column_list}
