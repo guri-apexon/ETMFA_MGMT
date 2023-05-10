4d4bd77b415fb5e1cd5f2e4e475f00afbd78bf9a
urvesh.patel <urvesh.patel@iqvia.com>
2023-05-10 12:46:10 +0530

    Parent_id removed

--- etmfa/crud/pd_nlp_entity_data.py
+++ etmfa/crud/pd_nlp_entity_data.py
@@ -18,7 +18,7 @@ class NlpEntityCrud(CRUDBase[NlpEntityDb, NlpEntityCreate, NlpEntityUpdate]):
         try:
             all_term_data = db.query(NlpEntityDb).filter(
                 NlpEntityDb.doc_id == doc_id, NlpEntityDb.link_id == link_id,
-                NlpEntityDb.hierarchy != 'document', NlpEntityDb.user_id == user_id).distinct(NlpEntityDb.parent_id,NlpEntityDb.dts).order_by(asc(NlpEntityDb.dts)).all()
+                NlpEntityDb.hierarchy != 'document', NlpEntityDb.user_id == user_id).distinct(NlpEntityDb.standard_entity_name,NlpEntityDb.dts).order_by(asc(NlpEntityDb.dts)).all()
 
         except Exception as ex:
             all_term_data = []
@@ -29,7 +29,7 @@ class NlpEntityCrud(CRUDBase[NlpEntityDb, NlpEntityCreate, NlpEntityUpdate]):
         try:
             all_term_data = db.query(NlpEntityDb).filter(
                 NlpEntityDb.doc_id == doc_id,NlpEntityDb.standard_entity_name != "",
-                NlpEntityDb.hierarchy != 'document', NlpEntityDb.user_id == user_id).distinct(NlpEntityDb.parent_id,NlpEntityDb.dts).order_by(asc(NlpEntityDb.dts)).all()
+                NlpEntityDb.hierarchy != 'document', NlpEntityDb.user_id == user_id).distinct(NlpEntityDb.standard_entity_name,NlpEntityDb.dts).order_by(asc(NlpEntityDb.dts)).all()
         except Exception as ex:
             all_term_data = []
             logger.exception("Exception in retrieval of data from table", ex)
