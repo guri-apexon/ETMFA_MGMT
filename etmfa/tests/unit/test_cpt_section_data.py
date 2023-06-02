2bd803f93c2213b3b47fb485311289624489cbb2
1151186 <mouneshwara.sugappa@iqvia.com>
2023-06-02 19:54:38 +0530

    test case fix

--- etmfa/tests/unit/test_cpt_section_data.py
+++ etmfa/tests/unit/test_cpt_section_data.py
@@ -4,11 +4,11 @@ from etmfa.server.namespaces.cptconfigapi import get_section_data
 
 
 @pytest.mark.parametrize("doc_id, link_level, link_id, user_id, protocol, status_code, comments", [
-    ("46d3456d-8aa2-4022-b251-b03271c151b3", '3', "c41f313b-9c40-456a-ac45-9c2e99aeabaa", "Dig2_Batch_Tester",
+    ("e7be9307-a16c-4237-9988-56a7e61aa4b7", '3', "486e2769-e4bb-11ed-96d9-005056ab6469", "Dig2_Batch_Tester",
      "cicl.06ed2096-0749-4ead-a892-9e57ead4fcbc", 200, "doc id and with link id and link_level 3"),
     ("1698be28-1cf3-466e-8f56-5fc920029056", "1", "", "1036048",
      "FEED_TEST4", 404, "doc id changes"),
-    ("46d3456d-8aa2-4022-b251-b03271c151b3", "1", "c41f313b-9c40-456a-ac45-9c2e99aeabaa", "Dig2_Batch_Tester",
+    ("e7be9307-a16c-4237-9988-56a7e61aa4b7", "1", "486e2769-e4bb-11ed-96d9-005056ab6469", "Dig2_Batch_Tester",
      "cicl.06ed2096-0749-4ead-a892-9e57ead4fcbc", 200, "doc id and link id present with level 1 all data"),
     ("4c7ea27b-8a6b-4bf0-a8ed-2c1e49bbdc8c", "1", "46bac1b7-9197-11ed-b507-005056ab6469", "Dig2_Batch_Tester",
      "005", 200, "doc id and link id with enriched data")
@@ -32,7 +32,7 @@ def test_document_object(new_app_context, user_id, protocol, doc_id, status_code
 
 
 @pytest.mark.parametrize("aidoc_id, link_level, link_id, user_id, protocol", [
-    ("46d3456d-8aa2-4022-b251-b03271c151b3", 3, "c41f313b-9c40-456a-ac45-9c2e99aeabaa", "Dig2_Batch_Tester",
+    ("e7be9307-a16c-4237-9988-56a7e61aa4b7", 3, "486e2769-e4bb-11ed-96d9-005056ab6469", "Dig2_Batch_Tester",
      "cicl.06ed2096-0749-4ead-a892-9e57ead4fcbc"),
     ("4c7ea27b-8a6b-4bf0-a8ed-2c1e49bbdc8c", 2, "46bac1b7-9197-11ed-b507-005056ab6469", "mgmt",
          "005")
